937b2d84413800de913e1f526ba5c5824cd08612
Bharati-jaybhay <155735053+bharati-jaybhay@users.noreply.github.com>
2025-02-20 13:38:26 +0530

    Delete online-voting directory

--- online-voting/src/main/java/com/sprk/sprk_hotels/repository/CandidateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sprk.sprk_hotels.repository;
-
-import com.sprk.sprk_hotels.model.Candidate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CandidateRepository extends JpaRepository<Candidate,Integer> {
-}
